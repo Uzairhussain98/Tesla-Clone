8364f1106177ca4578bee82ea307e05f51dee6db
Uzair Hussain <uzairhussain98@hotmail.com>
2021-06-07 01:15:45 +0500

    completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React , {useState} from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu';
-
+import CloseIcon from '@material-ui/icons/Close';
+import {selectCars} from '../features/Car/carSlice'
+import {useSelector} from 'react-redux'
 
 const Header = () => {
+
+  const [status , setStatus ] = useState(false);
+  const cars = useSelector(selectCars);
+  console.log(cars);
+
   return (
     <Container>
       <a>
@@ -11,10 +18,13 @@ const Header = () => {
       </a>
 
       <Menu>
-        <a href="#">Model S</a>
-        <a href="#">Model 3</a>
-        <a href="#">Model X</a>
-        <a href="#">Model Y</a>
+        { cars && cars.map((car , index) => (
+
+          <a key={index} href="#">{car}</a>
+
+        ) )}
+
+      
       </Menu>
 
       <RightMenu>
@@ -22,14 +32,19 @@ const Header = () => {
       <a href="#">Shop</a>
       <a href="#">Tesla Account</a>
 
-      <CustomMenu/>
+      <CustomMenu onClick={() => setStatus(true)}/>
 
 
 
 
       </RightMenu>
 
-      <BurgerNav>
+      
+      <BurgerNav show={status}>
+        <CloseWrap>
+        <CustomCloseBtn  onClick={() => setStatus(false)}/>
+        </CloseWrap>
+
       <li><a href="#">Existing Inventory</a></li>
       <li><a href="#">Used Inventory</a></li>
       <li><a href="#">Trade-In</a></li>
@@ -40,6 +55,7 @@ const Header = () => {
 
 
       </BurgerNav>
+
 
 
     </Container>
@@ -57,7 +73,8 @@ const Container  = styled.div`
   padding 0 20px;
   top : 0 ;
   left : 0 ;
-  right : 0
+  right : 0;
+  z-index : 1;
 
 `
 
@@ -96,11 +113,39 @@ cursor : pointer;
 `
 
 const BurgerNav  = styled.div`
+display: flex ;
 position: fixed;
 top :0;
 bottom  :0;
 right : 0;
 background : white;
 width :300px;
-z-index: 100;
+z-index: 16;
+list-style: none;
+padding: 20px;
+flex-direction: column ;
+text-align : start;
+transform : ${props => props.show ? 'translateX(0)' : 'translateX(100%)' } ;
+transition : transform 0.2s ease-in-out;
+
+li{
+  padding 15px 0 ;
+  border-bottom : 1px solid rgba(0,0,0,.2);
+  a{
+    font-weight: 600;
+  }
+
+}
+
 `
+
+const CustomCloseBtn  = styled(CloseIcon)`
+  cursor: pointer;
+`
+const CloseWrap  = styled.div`
+display: flex;
+justify-content: flex-end;
+
+`
+
+
